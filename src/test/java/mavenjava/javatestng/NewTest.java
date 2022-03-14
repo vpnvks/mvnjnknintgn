b909afed85073d276f4d8424ce890968fb4e4bf9
Vipin K Mishra <vipimira@in.ibm.com>
2022-03-14 23:37:41 +0530

    add myfile

---
+++ src/test/java/mavenjava/javatestng/NewTest.java
@@ -0,0 +1,11 @@
+package mavenjava.javatestng;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("this is my first test case");
+	  System.out.println("this is my first test case2");
+  }
+}
